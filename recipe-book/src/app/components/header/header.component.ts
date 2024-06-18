0b6e2c645082ef3b8352300838786aa990605d81
mnash29 <mtntrades10@gmail.com>
2024-06-17 17:00:52 -0700

    Implemented basic databinding for navbar selection

--- recipe-book/src/app/components/header/header.component.ts
+++ recipe-book/src/app/components/header/header.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  @Output() navBarItemClickEmitter = new EventEmitter<string>();
 
+  constructor() {}
+  
+  ngOnInit(): void {}
+
+  onNavBarClicked(navBarItem: string) {
+    this.navBarItemClickEmitter.emit(navBarItem)
+  }
 }
