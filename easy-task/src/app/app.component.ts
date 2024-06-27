724d5cd3e98a52f440e874b223e4e29faacb10bf
mnash29 <mtntrades10@gmail.com>
2024-06-26 22:46:49 -0700

    Added tasks component

--- easy-task/src/app/app.component.ts
+++ easy-task/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  selectedUser: any;
+
+  onSelectUser(id: string) {
+    this.selectedUser = DUMMY_USERS.find((user) => user.id === id);
+  }
+}
--- easy-task/src/app/user/user.component.ts
+++ easy-task/src/app/user/user.component.ts
@@ -1,8 +1,8 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, input, computed, output } from '@angular/core';
 
-import { DUMMY_USERS } from '../dummy-users';
+// import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -12,28 +12,37 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // selectedUser = DUMMY_USERS[randomIndex];
+  // Alertnative to `Output` decorator
+  readonly didSelectedUser = output<string>();
+  // @Output({required: true}) didSelectedUser = new EventEmitter();
 
   // Using signals, Angular is notified what a change occurs to update
   // all components of the app using this `signal` (change detection)
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
 
-  // Using computed, the value only changes when the underlying signal is invoked
-  selectedUserAvatarPath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  // Using `computed`, the value only changes when the underlying signal is invoked
+  // alternative to using `getter` method
+  selectedUserAvatarPath = computed(() => 'assets/users/' + this.avatar());
+
+  // Alternative to `Input` decorator and exposed as a `Signal`
+  readonly avatar = input.required<string>();
+  readonly name = input.required<string>();
+  readonly id = input.required<string>();
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
 
   /**
-   * getter for selected user avatar path
+   * getter for selected user avatar path but is triggered whenever the component
+   * is changed and not just the avatar property. See `computed`
    */
   // get selectedUserAvatarPath() {
-  //   return 'assets/users/' + this.selectedUser().avatar;
+  //   return 'assets/users/' + this.avatar;
   // }
 
   /**
    * Event listener for user button clicked
    */
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    // this.selectedUser = DUMMY_USERS[randomIndex];
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    this.didSelectedUser.emit(this.id());
   }
 }
