25fe4606a93acc68fd767dabff3981f7d47d4be6
mnash29 <mtntrades10@gmail.com>
2024-07-25 15:42:46 -0700

    Added header component

--- investment-calculator/src/app/app.module.ts
+++ investment-calculator/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
-  imports: [],
+  imports: [BrowserModule, HeaderComponent],
 })
 export class AppModule {}
