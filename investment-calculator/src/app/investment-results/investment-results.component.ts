cc6f37c55baf5fd0c2051032632498436d54dcc7
mnash29 <mtntrades10@gmail.com>
2024-08-02 15:02:43 -0700

    Added currency pipe

--- investment-calculator/src/app/investment-results/investment-results.component.ts
+++ investment-calculator/src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { AnnualData } from '../models/investment.model';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
