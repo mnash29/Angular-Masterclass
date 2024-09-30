e16084048bb4520368b50bd1e39f148ed1236deb
mnash29 <mtntrades10@gmail.com>
2024-09-30 16:07:22 -0700

    Added basic slide-in-out button effects

--- advanced-css/src/app/app.component.ts
+++ advanced-css/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TransitionsComponent } from "./components/transitions/transitions.component";
 import { TransformsComponent } from "./components/transforms/transforms.component";
+import { ButtonsComponent } from './components/buttons/buttons.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TransitionsComponent, TransformsComponent],
+  imports: [RouterOutlet, TransitionsComponent, TransformsComponent, ButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
