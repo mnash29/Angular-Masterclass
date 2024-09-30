c505faec059aaa57843f66a894d0316ec4cc9060
mnash29 <mtntrades10@gmail.com>
2024-09-30 11:54:24 -0700

    Added 2D transform examples

--- advanced-css/src/app/app.component.ts
+++ advanced-css/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TransitionsComponent } from "./components/transitions/transitions.component";
+import { TransformsComponent } from "./components/transforms/transforms.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TransitionsComponent],
+  imports: [RouterOutlet, TransitionsComponent, TransformsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ advanced-css/src/app/components/transforms/transforms.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transforms',
+  standalone: true,
+  imports: [],
+  templateUrl: './transforms.component.html',
+  styleUrl: './transforms.component.css'
+})
+export class TransformsComponent {
+
+}
