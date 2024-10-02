205e5abdd79eb756e8270365e07ba56fa237fe62
mnash29 <mtntrades10@gmail.com>
2024-10-02 15:56:13 -0700

    Created animations component

--- advanced-css/src/app/app.component.ts
+++ advanced-css/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { TransitionsComponent } from "./components/transitions/transitions.component";
 import { TransformsComponent } from "./components/transforms/transforms.component";
 import { ButtonsComponent } from './components/buttons/buttons.component';
+import { AnimationsComponent } from "./components/animations/animations.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TransitionsComponent, TransformsComponent, ButtonsComponent],
+  imports: [RouterOutlet, TransitionsComponent, TransformsComponent, ButtonsComponent, AnimationsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
