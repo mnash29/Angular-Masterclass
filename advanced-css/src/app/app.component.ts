8cd2ffa37aeb3d6a16f9422d2855d50a0d8c50cf
mnash29 <mtntrades10@gmail.com>
2024-10-09 16:05:19 -0700

    Added wave animation clip path

--- advanced-css/src/app/app.component.ts
+++ advanced-css/src/app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TransitionsComponent } from "./components/transitions/transitions.component";
-import { TransformsComponent } from "./components/transforms/transforms.component";
+import { TransitionsComponent } from './components/transitions/transitions.component';
+import { TransformsComponent } from './components/transforms/transforms.component';
 import { ButtonsComponent } from './components/buttons/buttons.component';
-import { AnimationsComponent } from "./components/animations/animations.component";
+import { AnimationsComponent } from './components/animations/animations.component';
+import { ClipPathComponent } from './components/clip-path/clip-path.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TransitionsComponent, TransformsComponent, ButtonsComponent, AnimationsComponent],
+  imports: [
+    RouterOutlet,
+    TransitionsComponent,
+    TransformsComponent,
+    ButtonsComponent,
+    AnimationsComponent,
+    ClipPathComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'advanced-css';
