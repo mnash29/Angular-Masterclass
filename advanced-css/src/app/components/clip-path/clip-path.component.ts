a848ddc291c91538d6d9d013e56fa5c7028abdb0
mnash29 <mtntrades10@gmail.com>
2024-10-09 13:33:04 -0700

    ng g c components/clip-path

---
+++ advanced-css/src/app/components/clip-path/clip-path.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clip-path',
+  standalone: true,
+  imports: [],
+  templateUrl: './clip-path.component.html',
+  styleUrl: './clip-path.component.css'
+})
+export class ClipPathComponent {
+
+}
